721a0b8080869dd13ea16642e29fc0313769ee45
Rajeev <telagasettirajeev999@gmail.com>
2023-06-25 20:37:34 +0530

    first and last commit after all changes..

---
+++ script.js
@@ -0,0 +1,19 @@
+
+let body = document.querySelector('.container');
+
+redClick = () => {
+    body.style.backgroundColor = "red";
+}
+yellowClick = () => {
+    body.style.backgroundColor = "yellow";
+}
+blueClick = () => {
+    body.style.backgroundColor = "blue";
+}
+greenClick = () => {
+    body.style.backgroundColor = "green";
+}
+
+resetClick = () => {
+    body.style.backgroundColor = "white";
+}
